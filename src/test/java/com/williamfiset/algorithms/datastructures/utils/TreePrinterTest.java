3bad94237120a08c78233c7f7b469c0d343f3da6
manjikian <51725316+manjikian@users.noreply.github.com>
2021-02-24 09:28:01 +0100

    #4 feat: Improve knn() in QuadTree (#19)
    * Add new unit tests for Improved coverage.
    
    * Update comments in tests.
    
    * Explain the refactoring approach in a comment
    
    * Refactor knn()
    Reduce cyclomatic complexity from 97 to 43
    
    Co-authored-by: Hovig <hovig@mail.com>

--- src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
+++ src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
@@ -1,36 +1,36 @@
-
 package com.williamfiset.algorithms.datastructures.utils;
+
 import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.*;
 
-
 public class TreePrinterTest {
-    private class TestTreeNode implements TreePrinter.PrintableNode {
-        String value;
-        private TestTreeNode left;
-        private TestTreeNode right;
-    
-        public TestTreeNode(String value, TestTreeNode left, TestTreeNode right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-    
-        // Get left child
-        public TreePrinter.PrintableNode getLeft() {
-            return this.left;
-        }
-    
-        // Get right child
-        public TreePrinter.PrintableNode getRight() {
-            return this.right;
-        }
-    
-        // Get text to be printed
-        public String getText() {
-            return value;
-        }
+  private class TestTreeNode implements TreePrinter.PrintableNode {
+    String value;
+    private TestTreeNode left;
+    private TestTreeNode right;
+
+    public TestTreeNode(String value, TestTreeNode left, TestTreeNode right) {
+      this.value = value;
+      this.left = left;
+      this.right = right;
     }
+
+    // Get left child
+    public TreePrinter.PrintableNode getLeft() {
+      return this.left;
+    }
+
+    // Get right child
+    public TreePrinter.PrintableNode getRight() {
+      return this.right;
+    }
+
+    // Get text to be printed
+    public String getText() {
+      return value;
+    }
+  }
 
   @Test
   public void treePrinter_displayTree_checkNodeValuesExist() {
